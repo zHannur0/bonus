2ada7b4f47c0e400cd28b8091b9b2efcdd4b9783
Zhannurkhan <zhannurkhan04@gmail.com>
2024-05-20 22:18:15 +0500

    npm run de

--- src/components/StoreList.jsx
+++ src/components/StoreList.jsx
@@ -1,14 +1,18 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
+import Link from "next/link";
+import {useRouter} from "next/router";
 
 
 
 const StoreList = ({members, value}) => {
+    const router = useRouter();
+    const id = String(router.query.id);
     return (
         <div className="p-4 h-[90%]">
             <h2 className="mb-4 text-lg font-medium text-black">Все партнеры</h2>
             {members.slice().filter((item)=> item.name.toLowerCase().includes(value.toLowerCase())).map((store, index) => (
-                <div key={store.id} className="flex items-center p-4 bg-white gap-4 justify-between">
+                <div key={store.memberId} className="flex items-center p-4 bg-white gap-4 justify-between">
                     <div className={"flex gap-4"}>
                         <img src={"/img/def.png"} alt={store.name} className="w-[65px] h-[65px]"/>
                         <div className="flex-col items-center">
@@ -16,7 +20,9 @@ const StoreList = ({members, value}) => {
                             <p className="text-[#818C99]">{store.address}</p>
                         </div>
                     </div>
-                    <img src="/img/shift.png" alt=""/>
+                    <Link href={`/${id}/${store.memberId}`}>
+                        <img src="/img/shift.png" alt=""/>
+                    </Link>
                 </div>
             ))}
         </div>
